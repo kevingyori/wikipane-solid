cc0c0f4b77f879a0427e95e0109f6bad93ac9e71
Kevin Gyori <gyorijonatan@gmail.com>
2024-06-03 21:20:33 +0100

    init

---
+++ src/routes/[...404].tsx
@@ -0,0 +1,11 @@
+import { A } from "@solidjs/router";
+
+export default function NotFound() {
+  return (
+    <div>
+      <A href="/" class="text-sky-600 hover:underline">
+        Go Home
+      </A>
+    </div>
+  );
+}
